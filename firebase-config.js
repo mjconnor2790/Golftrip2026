2fe8ba7bb9b82cbb13de3ce81c0d5662a29369f5
mjconnor2790 <mjconnor2@outlook.com>
2026-07-24 09:27:07 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDEda6wGqTi_u0IAnLCvqm_4Qdr-d2rroo",
-  authDomain: "amendoeira-cup.firebaseapp.com",
-  databaseURL: "https://amendoeira-cup-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "amendoeira-cup",
-  storageBucket: "amendoeira-cup.firebasestorage.app",
-  messagingSenderId: "895446145750",
-  appId: "1:895446145750:web:18258980d2294008090777"
-};
